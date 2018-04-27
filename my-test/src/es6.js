036bc988cf780eb465b35650c26c638b49519017
贾强-公司PC <jiaqiang17@126.com>
2018-04-27 11:47:42 +0800

    添加代码

--- my-test/src/es6.js
+++ my-test/src/es6.js
@@ -1,2 +1,18 @@
-let ss = (x => x*3)(3);
-console.log(ss);
+var babel = require('babel-core');
+
+// 字符串转码
+babel.transform("code();", options);
+// => {code, map, ast}
+
+// 文件转码(异步)
+babel.transformFile('filename.js', options, function (err, result) {
+    result;
+});
+
+// 文件转码(同步)
+babel.transformFile('filename.js', options);
+// => { code, map, ast }
+
+// Babel AST转码
+babel.transformFromAst(ast, code, options);
+// => { code, map, ast }
